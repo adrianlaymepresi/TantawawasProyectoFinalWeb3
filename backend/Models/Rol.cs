74d3ebcf301a52aedea174f97811169c3402dfa1
adrianlaymepresi <ignacioadrianlaymedelgado@gmail.com>
2025-11-20 21:12:39 -0400

    AdrianLayme: Modelo Rol inicial

---
+++ backend/Models/Rol.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Models
+{
+    public class Rol
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(50)]
+        public string NombreRol { get; set; } = string.Empty;
+    }
+}
