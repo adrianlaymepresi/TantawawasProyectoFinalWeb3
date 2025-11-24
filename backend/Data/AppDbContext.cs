249ec30005645d8ea02cdbf674d1a27dea6061cf
adrianlaymepresi <ignacioadrianlaymedelgado@gmail.com>
2025-11-24 16:25:41 -0400

    AdrianLayme: Creacion del Context Inicial

---
+++ backend/Data/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using backend.Models;
+
+namespace backend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Rol> Roles => Set<Rol>();
+        public DbSet<Usuario> Usuarios => Set<Usuario>();
+        public DbSet<Curso> Cursos => Set<Curso>();
+        public DbSet<Inscripcion> Inscripciones => Set<Inscripcion>();
+        public DbSet<Material> Materiales => Set<Material>();
+        public DbSet<Evaluacion> Evaluaciones => Set<Evaluacion>();
+        public DbSet<ResultadoEvaluacion> ResultadosEvaluaciones => Set<ResultadoEvaluacion>();
+        public DbSet<Mensaje> Mensajes => Set<Mensaje>();
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
